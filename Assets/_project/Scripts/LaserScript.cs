b58e8ffb422abef7fe6bcc427ea200d4940b67d8
RaheelParvez <21853@students.isb.ac.th>
2023-10-30 10:07:20 +0700

    Shooting Script
    Very cool shooting lol

--- Assets/_project/Scripts/LaserScript.cs
+++ Assets/_project/Scripts/LaserScript.cs
@@ -20,12 +20,9 @@ public class LaserScript : MonoBehaviour
         {
             Shoot();
             audioPlayer.Play();
+            Invoke("StopShooting", 0.1f);
         }
 
-        if(Input.GetMouseButtonUp(0))
-        {
-            StopShooting();
-        }
     }
 
     void Shoot()
